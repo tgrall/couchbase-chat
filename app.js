c4f92320c0265cb1ca585e1584dab3692243d9a3
Tugdual Grall <tugdual@gmail.com>
2012-10-14 07:30:32 +0200

    fix HTML, logout, and other functions names

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ var cb = new driver.Couchbase("127.0.0.1:8091", null, null, "default");
 
 server.listen(8080);
 
-
+/**
+ * Insert the view in the database if not already present
+ */
 function start() {
 	// check if the couchbase view exists 
  	// if not create it
@@ -29,11 +31,8 @@ function start() {
 		}
 		
 	});	
-	
-	
-
 }
-exports.start = start;
+
 start();
 
 
@@ -41,12 +40,15 @@ app.get('/', function(req, res) {
 	res.sendfile(__dirname + '/index.html');
 });
 
-// usernames which are currently connected to the chat
-var usernames = {};
+var usernames = {}; // contains the list of connected users
+
 
 io.sockets.on('connection', function(socket) {
-	
-	socket.on('sendchat', function(data) {
+
+	/**
+	 * When the user post a new message this even it called
+	 */
+	socket.on('postMessage', function(data) {
 		// create a new message
 		var message = {
 			type: "message",
@@ -55,17 +57,24 @@ io.sockets.on('connection', function(socket) {
 			timestamp: Date.now()
 		}
 		var messages = [message];
-		io.sockets.emit('updatechat', messages);		
-		persistData(message);
+		io.sockets.emit('updateChatWindow', messages);		
+
+		var messageKey = message.timestamp +"-"+ message.user;
+		cb.set(messageKey,  JSON.stringify(message) , 0, undefined, function(data, error, key, cas) {
+			if (error) {
+				console.log("Failed to store object");
+			} else {
+				if (key != messageKey) {
+					console.log("Callback called with wrong key!");
+				}
+			}
+		});
+
 	});
 
-
-	/*
-	 * This function is used to 'connect' the user to the chat server
-	 */
-	socket.on('connectuser', function(username) {
-		socket.username = username; // store the username in the socket session
-		usernames[username] = username; // add the user to the list of user
+	socket.on('addUser', function(username) {
+		socket.username = username; 
+		usernames[username] = username; 
 		
 		// create a new message
 		var message = {
@@ -75,33 +84,41 @@ io.sockets.on('connection', function(socket) {
 			timestamp: Date.now()
 		}
 		var messages = [message];
-		socket.emit('updatechat', messages); // send back a message to the suer
+		socket.emit('updateChatWindow', messages); 
 		
-		// create a new message
+		console.log( message );
+		
 		message = {
 			type: "message",
 			user: "SERVER",
 			message: username + " has connected",
 			timestamp: Date.now()
 		}
-		
-		socket.broadcast.emit('updatechat', message ); // broadcast to everybody
-		io.sockets.emit('updateusers', usernames); // update the userlist on the client
+		messages = [message];
+		socket.broadcast.emit('updateChatWindow', messages ); 
+		io.sockets.emit('updateUserWindow', usernames);
 
 
 	});
 
-	// when the user disconnects.. perform this
 	socket.on('disconnect', function() {
-		// remove the username from global usernames list
 		delete usernames[socket.username];
-		// update list of users in chat, client-side
-		io.sockets.emit('updateusers', usernames);
-		// echo globally that this client has left
-		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+		var message = {
+			type: "message",
+			user: "SERVER",
+			message: socket.username + " has disconnected",
+			timestamp: Date.now()
+		}
+		var messages = [message];
+		socket.broadcast.emit('updateChatWindow', messages ); 
+		io.sockets.emit('updateUserWindow', usernames);
 	});
 	
-	
+	/**
+	 * Call the view to get the older messages
+	 *  - startkey : the timestamp used as starting point to read the result 
+	 *  - limit : number of document/messages to retrieve
+	 */
 	socket.on('showhistory', function(limit,startkey) {
 		if (limit == undefined) {
 			limit = 5;
@@ -125,26 +142,12 @@ io.sockets.on('connection', function(socket) {
 					messages[i] = message;
 					// send the first key to the client
 					if(i == data.rows.length-1){
-						console.log(">>>> "+ message.message +" -- "+ message.timestamp);
-						io.sockets.emit('updateStartkey', message.timestamp - 1); // remove 1ms to the key
+						socket.emit('updateStartkey', message.timestamp - 1); // remove 1ms to the key
 					}
 				}	
-				io.sockets.emit('updatechat', messages, true); // show history
+				socket.emit('updateChatWindow', messages, true); // show history
 			});			
 	  	});
 });
 
 
-/** NEED TO SEE WHAT IS THE BEST WAY TO CALL A METHOD **/
-function persistData( message) {
-	var messageKey = message.timestamp +"-"+ message.user;
-	cb.set(messageKey,  JSON.stringify(message) , 0, undefined, function(data, error, key, cas) {
-		if (error) {
-			console.log("Failed to store object");
-		} else {
-			if (key != messageKey) {
-				console.log("Callback called with wrong key!");
-			}
-		}
-	});
-}
